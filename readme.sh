259546aebf55570c47c84b80e1d21923dba1a6e4
Sathvik PN <sathvikapn@gmail.com>
2025-06-09 19:14:25 +0530

    add bash func to automate cs50 code distribution download

--- readme.sh
+++ readme.sh
@@ -14,5 +14,31 @@ else
     exit 1
 fi
 
+function get_cs50_code() {
+    # Check if the CS50 code is provided as an argument
+    if [ -z "$1" ]; then
+        echo "Example: get_cs50_code https://cs50.harvard.edu/ai/2024/projects/0/degrees/degrees.zip"
+        return 1
+    fi
+
+    # Extract the filename from the URL (everything after the last '/')
+    local zip_file="${1##*/}"
+
+    # Download the zip file
+    curl -L -o "$zip_file" "$1" &&
+
+    # Unzip the file
+    unzip -o "$zip_file" && 
+
+    # Delete the original zip file
+    rm -f "$zip_file"  && 
+
+    # print the folder name that was created without .zip extension
+    echo &&
+    echo "downloaded: ${zip_file%.zip}/" 
+    echo
+}
+
+
 # NOTES 
 # view CS50 AI - https://cs50.harvard.edu/ai/2024/projects/0/degrees/
